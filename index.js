7c100bf52291e3b2e39eb4279c163b6689f981b9
telemacowill <aglwill@gmail.com>
2023-12-28 09:46:26 -0300

    post-out-path-delet methods

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('Hello World');
+app.get('/', (_req, res) => {
+    return res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+    const data = req.body;
+    console.log(data);
+    return res.end();
+});
+
+app.put('/', (_req, res) => {
+    
+    return res.end('Put Request');
+});
+
+app.path('/', (_req, res) => {
+
+    return res.end('Path Request');
+});
+
+app.delete('/', (_req, res) => {
+
+    return res.end();
 });
 
 app.listen(3000, () =>
