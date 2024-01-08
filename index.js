9255851d7693b3bab24ed66db9a22cc4d8f306cd
telemacowill <aglwill@gmail.com>
2024-01-08 08:06:49 -0300

    add aux variable and IF

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import express from "express";
+import { randomUUID } from "node:crypto";
 
 const app = express();
 app.use(express.json());
 
+const data = {};
+
 app.get('/', (_req, res) => {
-    return res.send('Hello World');
+    return res.json(data);  
 });
 
 app.post('/', (req, res) => {
-    const data = req.body;
+    //const data = req.body;
+    let aux = req.body;
+    let objaux = {};
+    aux.id = randomUUID();
     console.log(data);
+    if(data.lenght > 0 ){
+        objaux={...data, aux};
+    }
+    else {
+        data = objaux;
+    }
+    data = aux;
+    
+    //console.log(data);
     return res.end();
 });
 
